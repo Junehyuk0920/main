12661763a65d9eaa58acbb506e98d305d0eb37ee
Junehyuk0920 <junehyuk0290@gmail.com>
2025-09-22 15:52:10 +0900

    mobile upd

--- mobile.js
+++ mobile.js
@@ -1,2 +1,6 @@
 if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent))
+{
     $(".circle").hide();
+    $(".p:not(#projects)").css("height", "500px");
+    $("#projects").css("min-height", "500px");
+}
