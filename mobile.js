89d7f25ae76107e8643058c839f3177afb107310
Junehyuk0920 <junehyuk0290@gmail.com>
2025-10-01 21:15:42 +0900

    MOBILE UPDATE

--- mobile.js
+++ mobile.js
@@ -1,18 +1,8 @@
-var vh_to_px;
-
-if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent))
+function scaleBody()
 {
-    $(".circle").hide();
-
-    function setPageHeight()
-    {
-        var winWidth = window.innerWidth;
-        vh_to_px = (765 / 1440) * winWidth;
-
-        $(".p:not(#projects)").css("height", `${vh_to_px}px`);
-        $("#projects").css("min-height", `${vh_to_px}px`);
-    }
-
-    setPageHeight();
-    $(window).on("resize orientationchange", setPageHeight);
+    var scale = $(window).width() / 1440;
+    $("body").css("transform", "scale(" + scale + ")");
 }
+
+scaleBody();
+$(window).on("resize", scaleBody);
