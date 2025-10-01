5b1a599a3d4573bc56443d8ae885ef1104e9b097
Junehyuk0920 <junehyuk0290@gmail.com>
2025-10-01 21:21:30 +0900

    upd

--- mobile.js
+++ mobile.js
@@ -1,7 +1,13 @@
 function scaleBody()
 {
-    var scale = $(window).width() / 1440;
-    $("body").css("transform", "scale(" + scale + ")");
+    var winWidth = $(window).width();
+    var scale = winWidth / 1440;
+
+    $("body").css({
+        "transform": "scale(" + scale + ")",
+        "width": 1440 / scale + "px",
+        "margin": "0 auto"
+    });
 }
 
 scaleBody();
