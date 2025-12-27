a37d1f36c3445c35d099b57cf8586ce03a93dc65
Junehyuk0920 <junehyuk0290@gmail.com>
2025-12-27 23:09:39 +0900

    project viewer ui edit

--- project/script.js
+++ project/script.js
@@ -1,7 +1,7 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const idx = urlParams.get("project");
+const idx = parseInt(urlParams.get("project"));
 var data;
 
 async function getData()
@@ -10,7 +10,7 @@ async function getData()
     {
         const response = await fetch("../data.json");
         data = await response.json();
-        data = data[0][idx];
+        data = data[0];
     } catch (error)
     {
         console.error("데이터를 불러오는 중 오류 발생:", error);
@@ -20,20 +20,21 @@ async function getData()
 var projectName = document.querySelector("h1");
 var projectDesc = document.querySelector("p");
 var projectImg = document.querySelector("img");
-var projectSkill = document.querySelector(".skills");
+var projectSkill = document.querySelector(".skills ul");
 
 function updateData()
 {
-    if(!data)
+    let dt = data[idx];
+    if(!dt)
     {
         alert("존재하지 않는 프로젝트입니다!");
         location.href = "../index.html";
     }
-    projectImg.src = data[0];
-    projectName.textContent = data[1];
-    projectDesc.innerHTML = (data[3] != "") ? data[3] : "<small>Instead of a long description, a short summary is provided below.</small><br><br>" + data[2];
+    projectImg.src = dt[0];
+    projectName.textContent = dt[1];
+    projectDesc.innerHTML = (dt[3] != "") ? dt[3] : dt[2] + "<br><br><small>Only Short Description</small>";
 
-    data[4].forEach(skill => {
+    dt[4].forEach(skill => {
         projectSkill.insertAdjacentHTML("beforeend", `<li>${skill}</li>`);
     })
 }
@@ -45,3 +46,17 @@ async function init()
 }
 
 init();
+
+document.querySelector(".home").addEventListener("click", () => {
+    location.href = "../index.html"
+})
+
+document.querySelector(".prev").addEventListener("click", () => {
+    if (idx > 0) location.href = `./index.html?project=${idx-1}`;
+    else alert("가장 처음 프로젝트입니다!");
+})
+
+document.querySelector(".next").addEventListener("click", () => {
+    if (idx < data.length-1) location.href = `./index.html?project=${idx+1}`;
+    else alert("가장 마지막 프로젝트입니다!");
+})
